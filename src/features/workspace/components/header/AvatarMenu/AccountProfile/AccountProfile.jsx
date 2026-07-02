433a692e33cc2bfba9b1870d7d1cf074330507ae
vincentlow02 <226811304+vincentlow02@users.noreply.github.com>
2026-07-02 16:29:50 +0800

    backup: preserve local UI changes before merging friend commit

--- src/features/workspace/components/header/AvatarMenu/AccountProfile/AccountProfile.jsx
+++ src/features/workspace/components/header/AvatarMenu/AccountProfile/AccountProfile.jsx
@@ -1,8 +1,16 @@
 import './AccountProfile.css';
+import editAvatarIcon from '../../../../../../assets/icons/account-edit-avatar.svg';
+import IconImage from '../Shared/IconImage';
+
 export default function AccountProfile() {
   return (
     <div className="avatar-menu__profile">
-      <div className="avatar-menu__avatar" />
+      <div className="avatar-menu__avatar-wrap">
+        <div className="avatar-menu__avatar" />
+        <button className="avatar-menu__avatar-edit" type="button" aria-label="Edit profile photo">
+          <IconImage src={editAvatarIcon} />
+        </button>
+      </div>
       <div className="avatar-menu__name">Q X</div>
       <div className="avatar-menu__email-wrap">
         <div className="avatar-menu__email">lowvincent8@gmail.com</div>
